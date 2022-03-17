e01c0240ed645475e5b0d1a9d14f18e72c9b22ca
Jason Kong <jasonxkong@icloud.com>
2022-03-17 22:49:38 +0800

    display & update User details And PrivateRoute Component & useAuthStatus Hook

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
 
 //import Toast message package
 import { ToastContainer } from "react-toastify";
@@ -20,7 +21,9 @@ function App() {
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/forgotPassword" element={<ForgotPassword />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Routes>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,31 +1,101 @@
 import React, { useState, useEffect } from "react";
-
-import { getAuth } from "firebase/auth";
 import { useNavigate, Link } from "react-router-dom";
+
+// Firebase
+import { getAuth, updateProfile } from "firebase/auth";
+import { db } from "../firebase.config";
+import { updateDoc, doc } from "firebase/firestore";
+
+import { toast } from "react-toastify";
+import { async } from "@firebase/util";
 
 function Profile() {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
-  const { email, password } = formData;
+  const { name, email } = formData;
 
   const onLogout = () => {
     auth.signOut();
     navigate("/");
   };
 
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //Update display name in firebase
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // Update in firestore
+        const userRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      toast.error("Could not update profile details");
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <div className="profile">
       <header className="profileHeader">
         <p className="pageHeader">My Profile</p>
-        <buuton className="logOut" type="button" onClick={onLogout}>
+        <button className="logOut" type="button" onClick={onLogout}>
           Logout
-        </buuton>
+        </button>
       </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              setChangeDetails((prevState) => !prevState);
+              // if change is true -> onSubmit
+              changeDetails && onSubmit();
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+      </main>
+
+      <div className="profileCard">
+        <form>
+          <input
+            type="text"
+            id="name"
+            className={!changeDetails ? "profileName" : "profileNameActive"}
+            disabled={!changeDetails}
+            value={name}
+            onChange={onChange}
+          />
+          <input
+            type="text"
+            id="email"
+            className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+            disabled={!changeDetails}
+            value={email}
+            onChange={onChange}
+          />
+        </form>
+      </div>
     </div>
   );
 }
